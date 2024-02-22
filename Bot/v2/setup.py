6da999da2ae9c3feb15cb962c9ca0dad39a86016
Svik192 <grigoriy.del@gmail.com>
2024-02-22 11:49:03 +0200

    объединение интерфейса и установка пакета с именем bot_pg15

---
+++ Bot/v2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='Bot',
+    version='1.1',
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    entry_points={'console_scripts': ['bot_pg15 = Bot.main:main']},
+    packages=find_namespace_packages(),
+
+)
